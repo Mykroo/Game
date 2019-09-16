15c05aa271a75a14406c104c184fb1a9f1013750
Mykro <mykro@Mykros-MacBook-Pro.local>
2019-09-16 17:59:23 -0500

    Message sending and receiving@

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('message', msg)
+	send(msg, broadcast = True)
+
+
+@app.route('/')
+def chat_room():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+	socketio.run(app.run(host='192.168.1.116',debug=True))
